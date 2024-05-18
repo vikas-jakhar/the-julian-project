88d0d208118e602b3a82e8795a394e79326c9efc
vikas-jakhar <vikashjakhar2004@gmail.com>
2024-05-18 15:31:19 +0530

    first commit

---
+++ src/js/app.js
@@ -0,0 +1,9 @@
+
+let title = document.title;
+window.addEventListener("blur", () => {
+    document.title = "Come back 🤞";
+});
+
+window.addEventListener("focus", () => {
+    document.title = title;
+});
